97a30945d259b2f7bef94986f847802ae7f65b8b
Yousef <76191379+wi@users.noreply.github.com>
2024-09-14 11:55:45 -0400

    lol

--- vision/App.js
+++ vision/App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Camera, useCameraPermission, useCameraDevice } from 'react-native-vision-camera';
 
 export default function App() {
+  const [facing, setFacing] = useState('back');
+  const { hasPermission, requestPermission } = useCameraPermission();
+
+  const device = useCameraDevice("back");
+  
+
+  const [messages, setMessages] = useState(["test", "test2"]);
+
+  if (!hasPermission) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="Grant Permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.halfScreen}>
+        <Camera style={styles.camera} device={device} isActive={true}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+              <Text style={styles.text}>Flip Camera</Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
+
+      <View style={styles.halfScreen}>
+        <Text style={styles.message}>Camera is ready!</Text>
+        {messages?.map((message, index) => (
+          <Text key={index} style={{marginLeft: 10}}>{message}</Text>
+        ))}
+
+        <Button onPress={e => console.log("click")} title='lol' style={styles.debugButton} />
+      </View>
     </View>
   );
 }
@@ -13,8 +51,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  halfScreen: {
+    flex: 1,
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  debugButton: {
+    position: 'absolute',
+    bottom: 0,
+    right: 20,
+  }
 });
