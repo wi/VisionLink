6c2045443fb99095c7bf5e2e5f6bd890a49f342d
Yousef <76191379+wi@users.noreply.github.com>
2024-09-15 00:11:16 -0400

    Feat: Voice Rec

--- vision/App.js
+++ vision/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Platform, PermissionsAndroid, Button, StyleSheet, Text, View, TextInput, SafeAreaView, Image, TouchableOpacity, ScrollView } from 'react-native';
 import {
   Camera,
   useCameraDevice,
@@ -9,12 +9,71 @@ import {
 } from 'react-native-vision-camera';
 import { useImageLabeler } from 'react-native-vision-camera-v3-image-labeling';
 import { useSharedValue, Worklets } from 'react-native-worklets-core';
+import Voice from '@react-native-voice/voice';
 
 
 export default function App() {
   const { hasPermission, requestPermission } = useCameraPermission();
   const [messages, setMessages] = useState([]);
   const labels = useSharedValue([]);
+  const [isListening, setIsListening] = useState(false);
+  const [recognizedText, setRecognizedText] = useState('');
+
+  useEffect(() => {
+    Voice.onSpeechResults = onSpeechResults;
+    Voice.onSpeechError = error => console.log('onSpeechError', error);
+    Voice.onSpeechStart = onSpeechStart;
+    Voice.onSpeechEnd = stopListening;
+
+    const androidPremissionChecking = async () => {
+      if(Platform.OS === 'android'){
+        const hasPermission = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.RECORD_AUDIO);
+        console.log('hasPermission', hasPermission);
+        const getService = await Voice.getSpeechRecognitionServices();
+        console.log('getService', getService);
+      }
+    }
+    androidPremissionChecking();
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const onSpeechResults = event => {
+    setRecognizedText(event.value[0]);
+  }
+
+  const onSpeechStart = event => {
+    setIsListening(true);
+  }
+
+  const startListening = async() => {
+    setIsListening(true);
+    try {
+      await Voice.start('en-US');
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  const stopListening = async() => {
+    try {
+      await Voice.stop();
+      Voice.removeAllListeners();
+      setIsListening(false);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  const sendMessage = () => {
+    if (recognizedText) {
+      setMessages([...messages, {text: recognizedText, sender: 'user'}]);
+      setRecognizedText('');
+    }
+  };
+  
 
   const [previousLabels, setPreviousLabels] = useState([]);
   
@@ -146,6 +205,34 @@ export default function App() {
 
         <Button onPress={e => console.log(labels.value)} title='lol' style={styles.debugButton} />
       </View>
+    <View style={styles.container}>
+      <SafeAreaView />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Type your message..."
+          value={recognizedText}
+          onChangeText={text => setRecognizedText(text)}
+        />
+        <TouchableOpacity
+          onPress={() => (isListening ? stopListening() : startListening())}
+          style={styles.voiceButton}>
+          {isListening ? (
+            <Text style={styles.voiceButtonText}>•••</Text>
+          ) : (
+            <Image
+              source={{
+                uri: 'https://cdn-icons-png.flaticon.com/512/4980/4980251.png',
+              }}
+              style={{width: 45, height: 45}}
+            />
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity onPress={sendMessage} style={styles.sendButton}>
+          <Text style={styles.sendButtonText}>Send</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
     </View>
   );
 }
@@ -184,5 +271,52 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 0,
     right: 20,
-  }
+  },
+  messagesContainer: {
+    padding: 10,
+  },
+  messageBubble: {
+    maxWidth: '70%',
+    marginVertical: 5,
+    borderRadius: 10,
+    padding: 10,
+  },
+  messageText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    borderTopWidth: 1,
+    borderColor: '#ccc',
+    backgroundColor: 'white',
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    padding: 10,
+    borderRadius: 20,
+    backgroundColor: '#EFEFEF',
+  },
+  voiceButton: {
+    marginLeft: 10,
+    fontSize: 24,
+  },
+  voiceButtonText: {
+    fontSize: 24,
+    height: 45,
+  },
+  sendButton: {
+    marginLeft: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: '#FF6969',
+    borderRadius: 20,
+  },
+  sendButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
 });
